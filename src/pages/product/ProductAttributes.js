5903b842ea4c592366bf3d7d463d3bd2e287f913
Your Name <vmecxvarishvili@gmail.com>
2022-10-04 23:11:37 +0400

    Day 20

--- src/pages/product/ProductAttributes.js
+++ src/pages/product/ProductAttributes.js
@@ -3,11 +3,6 @@ import React from 'react';
 const ProductAttributes = (props) => {
     const attr = props.attr
     return (
-        attr.items ? 
-        <div>
-            s
-        </div>
-        :
         <div className="attrBox">
             <div className="id" >{attr.displayId}</div>
             <div className="value" >{attr.value}</div>
